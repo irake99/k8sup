7ef660673437aa0a425bf33e83e12506a8a64606
hsfeng <hsfeng@gmail.com>
2016-07-26 17:03:08 +0800

    Update entrypoint.sh & README.md

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Using One Docker container to bootstrap a HA Kubernetes cluster
 
 `$ docker build -t k8sup .`
 
-`$ docker run -it --privileged --net=host --pid=host -v /:/rootfs -v /bin/docker:/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /usr/lib/libdevmapper.so.1.02:/usr/lib/libdevmapper.so.1.02 -v /dev:/dev -v /sys:/sys -v /etc/cni:/etc/cni -v /opt/cni:/opt/cni -v /var/lib/cni:/var/lib/cni -v /var/lib/etcd:/var/lib/etcd k8sup 192.168.32.56`
+`$ docker run -it --privileged --net=host --pid=host -v /:/rootfs -v /bin/docker:/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /usr/lib/libdevmapper.so.1.02:/usr/lib/libdevmapper.so.1.02 -v /dev:/dev -v /sys:/sys -v /etc/cni:/etc/cni -v /opt/cni:/opt/cni -v /var/lib/cni:/var/lib/cni -v /var/lib/etcd:/var/lib/etcd k8sup <host-ip>`
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ sh -c 'docker stop flannel0' >/dev/null 2>&1 || true
 sh -c 'docker rm flannel0' >/dev/null 2>&1 || true
 
 echo "Running etcd"
-etcdCID=$(docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -v /var/lib/etcd:/var/lib/etcd -p 4001:4001 -p 2380:2380 -p 2379:2379 --restart=always --name etcd0 quay.io/coreos/etcd /usr/local/bin/etcd --name etcd0  --advertise-client-urls http://192.168.32.56:2379,http://192.168.32.56:4001  --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001  --initial-advertise-peer-urls http://192.168.32.56:2380  --listen-peer-urls http://0.0.0.0:2380  --initial-cluster-token etcd-cluster-1  --initial-cluster etcd0=http://192.168.32.56:2380  --initial-cluster-state new --data-dir /var/lib/etcd)
+etcdCID=$(docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -v /var/lib/etcd:/var/lib/etcd -p 4001:4001 -p 2380:2380 -p 2379:2379 --restart=always --name etcd0 quay.io/coreos/etcd /usr/local/bin/etcd --name etcd0  --advertise-client-urls http://$1:2379,http://$1:4001  --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001  --initial-advertise-peer-urls http://$1:2380  --listen-peer-urls http://0.0.0.0:2380  --initial-cluster-token etcd-cluster-1  --initial-cluster etcd0=http://$1:2380  --initial-cluster-state new --data-dir /var/lib/etcd)
 
 echo "Setting etcd"
 docker exec -it ${etcdCID} /usr/local/bin/etcdctl set /coreos.com/network/config '{ "Network": "10.1.0.0/16", "Backend": { "Type": "vxlan"}}'
 
 echo "Running flanneld"
-docker run --name flannel0 --net=host -d --privileged -v /dev/net:/dev/net -v /run/flannel:/run/flannel --restart=always quay.io/coreos/flannel:0.5.5 /opt/bin/flanneld --etcd-endpoints=http://192.168.32.56:4001
+docker run --name flannel0 --net=host -d --privileged -v /dev/net:/dev/net -v /run/flannel:/run/flannel --restart=always quay.io/coreos/flannel:0.5.5 /opt/bin/flanneld --etcd-endpoints=http://$1:4001
 
 echo "Running Kubernetes"
-/go/kube-up $@
+/go/kube-up $1
