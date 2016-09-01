8382c11c1552802daa1f60042eb4b7590cdadf68
Jacky <irake99@gmail.com>
2016-09-01 10:45:53 +0800

    Add an atomic operation for ensuring only one node can upload certs.

--- cp-certs.sh
+++ cp-certs.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
 function check_certs_exist_on_etcd(){
-  local ETCD_PATH="k8sup/cluster/k8s_certs"
+  local ETCD_PATH="$1"
+  local ERROR_CODE=""
+  local CERTS_ON_ETCD=""
 
-  if curl -sf "http://localhost:2379/v2/keys/${ETCD_PATH}" &>/dev/null; then
-    local CERTS_ON_ETCD="true"
+  until curl -s "http://127.0.0.1:2379/v2/keys" &>/dev/null; do
+    echo "Waiting for etcd ready..." 1>&2
+    sleep 1
+  done
+
+  # Atomic operation for ensuring there are no certs on etcd that uploaded by other node before.
+  curl -sf "http://127.0.0.1:2379/v2/keys/${ETCD_PATH}?prevExist=false" -XPUT -d dir=true 1>&2
+  ERROR_CODE="$?"
+  # Error code 22 means the certs have alreay been uploaded by other node before
+  if [[ "${ERROR_CODE}" == "22" ]]; then
+    CERTS_ON_ETCD="true"
+  elif [[ "${ERROR_CODE}" == "0" ]]; then
+    CERTS_ON_ETCD="false"
   else
-    local CERTS_ON_ETCD="false"
+    echo "Connect to etcd error, exiting..." 1>&2
+    exit "${ERROR_CODE}"
   fi
 
   echo "${CERTS_ON_ETCD}"
 }
 
 function upload_kube_certs(){
+  local ETCD_PATH="$1"
   local CERTS_DIR="/data"
   local FILE_LIST="ca.crt kubecfg.crt kubecfg.key server.cert server.key basic_auth.csv known_tokens.csv"
-  local ETCD_PATH="k8sup/cluster/k8s_certs"
+  local ENCODED_DATA=""
 
   # Wait a moment until all files exist
   local CERTS_EXISTED="false"
@@ -29,24 +44,23 @@ function upload_kube_certs(){
     fi
   done
 
-  curl -s "http://localhost:2379/v2/keys/${ETCD_PATH}" -XPUT -d dir=true 1>&2
   for FILE in ${FILE_LIST}; do
-    local ENCODED_DATA="$( cat "${CERTS_DIR}/${FILE}" | base64)"
-    curl -s "http://localhost:2379/v2/keys/${ETCD_PATH}/${FILE}" -XPUT -d value="${ENCODED_DATA}" &>/dev/null
+    ENCODED_DATA="$(cat "${CERTS_DIR}/${FILE}" | base64)"
+    curl -s "http://127.0.0.1:2379/v2/keys/${ETCD_PATH}/${FILE}" -XPUT -d value="${ENCODED_DATA}" &>/dev/null
   done
 }
 
 function download_kube_certs(){
+  local ETCD_PATH="$1"
   local CERTS_DIR="/data"
   local FILE_LIST="ca.crt kubecfg.crt kubecfg.key server.cert server.key basic_auth.csv known_tokens.csv"
-  local ETCD_PATH="k8sup/cluster/k8s_certs"
   local RAWDATA=""
   local CERT=""
 
    mkdir-p "${CERTS_DIR}"
 
   for FILE in ${FILE_LIST}; do
-    until RAWDATA="$(curl -sf "http://localhost:2379/v2/keys/${ETCD_PATH}/${FILE}")"; do
+    until RAWDATA="$(curl -sf "http://127.0.0.1:2379/v2/keys/${ETCD_PATH}/${FILE}")"; do
       echo "Waiting to get etcd keys..." 1>&2
       sleep 1
     done
@@ -70,11 +84,13 @@ function download_kube_certs(){
 }
 
 function main(){
-  local CERTS_ON_ETCD="$(check_certs_exist_on_etcd)"
+  local ETCD_PATH="k8sup/cluster/k8s_certs"
+  local CERTS_ON_ETCD=""
+  CERTS_ON_ETCD="$(check_certs_exist_on_etcd "${ETCD_PATH}")" || exit
   if [[ "${CERTS_ON_ETCD}" == "true" ]]; then
-    download_kube_certs
+    download_kube_certs "${ETCD_PATH}"
   else
-    upload_kube_certs &
+    upload_kube_certs "${ETCD_PATH}" &
   fi
 
   /setup-files.sh "$@"
